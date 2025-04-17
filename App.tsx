5200387783f7a3a2453c395346f71ed872109ece
huukhuong <huukhuong.it@gmail.com>
2025-04-17 13:10:09 +0700

    feat: config path alias, base folder structure

--- App.tsx
+++ App.tsx
@@ -1,12 +1,8 @@
 import React from 'react';
-import { View } from 'react-native';
 import './global.css';
 
 const App = () => {
-  return (
-    <View className='flex-1 items-center justify-center bg-red-500'>
-    </View>
-  );
+  return <></>;
 };
 
 export default App;
--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,27 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset', 'nativewind/babel'],
-  plugins: ['react-native-reanimated/plugin'],
+  plugins: [
+    'react-native-reanimated/plugin',
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@': './src',
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@constants': './src/constants',
+          '@hooks': './src/hooks',
+          '@navigation': './src/navigation',
+          '@screens': './src/screens',
+          '@services': './src/services',
+          '@store': './src/store',
+          '@theme': './src/theme',
+          '@types': './src/types',
+          '@utils': './src/utils'
+        }
+      }
+    ]
+  ]
 };
